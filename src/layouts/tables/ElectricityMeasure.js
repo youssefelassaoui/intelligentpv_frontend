b57bd1e7e60d9fd8df9e9d3b06abc2cdc41fbf29
youssef <youssef.elassaoui@gmail.com>
2024-11-11 16:10:09 +0100

    updates

--- src/layouts/tables/ElectricityMeasure.js
+++ src/layouts/tables/ElectricityMeasure.js
@@ -186,7 +186,7 @@ const MeasureLineChart = ({ plantId }) => {
   }, [plantId, selectedDeviceId, startDate, endDate, selectedPhase]);
 
   return (
-    <Card sx={{ height: "310px", width: "97%", marginTop: "18px", marginLeft: "21px" }}>
+    <Card sx={{ height: "500px", width: "97%", marginTop: "18px", marginLeft: "21px" }}>
       <MDBox p={2}>
         <h3>Electricity (Ampere)</h3>
         <Grid container spacing={2} alignItems="center">
@@ -248,7 +248,7 @@ const MeasureLineChart = ({ plantId }) => {
 
         <MDBox
           mt={2}
-          sx={{ height: "100%", width: "98%", overflowX: "scroll", position: "relative" }}
+          sx={{ height: "310px", width: "98%", overflowX: "scroll", position: "relative" }}
         >
           {loading ? (
             <MDBox
